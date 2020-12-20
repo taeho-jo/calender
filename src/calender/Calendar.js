2f851988acf530f312b69bbcb6373d6d3e850c46
taeho-jo <jotang3726@gmail.com>
2020-12-20 21:50:10 +0900

    코드 정리

--- src/calender/Calendar.js
+++ src/calender/Calendar.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import './calendar.scss'
 import moment from "moment";
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-
+// 오늘 날짜
 const TODAY = moment().format('YYYY-MM-DD')
+// 오늘 년 월
 const CUREENT_MONTH = moment().format('YYYY-MM')
 
+// api 호출 전 dummy 데이터
 const list= ["0", "0", "0", "0", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "0", "0", "0", "0", "0", "0", "0", "1", "0", "0", "0", "0", "0", "0", "0"]
 
 const Calendar = () => {
 
-  // 선택된 시작날짜와 마지막 날짜
+  // 선택된 시작 날짜와 마지막 날짜
   const [selectDate, setSelectDate] = useState({
     startDate: '',
     endDate: ''
   })
 
-  // 한달 시작과 끝 날짜
+  // 한달 시작 날짜와 마지막 날짜
   const [monthDate, setMonthDate] = useState({
     startDate: moment(`${CUREENT_MONTH}-01`).format('YYYY-MM-DD'),
     endDate: moment(`${CUREENT_MONTH}-01`).add(1, 'M').add(-1, 'd').format('YYYY-MM-DD')
   })
+  // 한달 시작 요일과 마지막 요일
   const [monthDay, setMonthDay] = useState({
     startDay: moment(`${CUREENT_MONTH}-01`).day(),
     endDay: moment(`${CUREENT_MONTH}-01`).add(1, 'M').add(-1, 'd').day()
@@ -30,76 +33,16 @@ const Calendar = () => {
   // 날짜 계산을 위한 덧셈 뺄셈 숫자
   const [ num, setNum ] = useState(0)
 
-  // 헤더 년 월
+  // 달력 제목 년 월
   const [standard, setStandard] = useState('')
 
   // 날짜 계산을 위한 기준 년 월 일
   const [dashStandard, setDashStandard] = useState('')
 
-  // 해당 월 날짜 배열
-  const [originDateArr, setOriginDateArr] = useState([])
-  // 날짜 모을 배열
+  // 모든 날짜를 넣는 배열
   const [dateArr, setDateArr] = useState([])
 
-  const test = () => {
-    const { startDate, endDate } = monthDate
-    const { startDay, endDay } = monthDay
-    const arr = []
-    const prevArr = []
-    const nextArr = []
-    const period = moment(endDate).diff(moment(startDate), "days");
-    for(let i = 0; i <= period; i++) {
-      let differencePeriod = moment(startDate).add(i, "d").format("DD");
-      arr.push({
-        date: differencePeriod,
-        origin: true,
-        able: selectAbleDate(i)
-      })
-    }
-
-    for(let i = 1; i <= startDay; i++) {
-      let pervPeriod = moment(startDate).add(-i, 'd').format('DD')
-      prevArr.unshift({
-        date: pervPeriod,
-        origin: false,
-        able: false
-      })
-    }
-
-    for(let i = 1; i <= 6 - endDay; i++) {
-      let nextPeriod = moment(endDate).add(i, 'd').format('DD')
-      nextArr.push({
-        date: nextPeriod,
-        origin: false,
-        able: true
-      })
-    }
-
-    setOriginDateArr(arr)
-    setDateArr([...prevArr, ...arr, ...nextArr])
-  }
-
-  // 상단 헤더 년도+날짜 계산
-  const getStandardDate = () => {
-    const year = moment(TODAY).add(num, 'M').format('YYYY')
-    const month = moment(TODAY).add(num, 'M').format('MM')
-    setStandard(`${year}년 ${month}월`)
-    setDashStandard(`${year}-${month}`)
-  }
-
-  const addMonth = () => {
-    setNum(num + 1)
-  }
-
-  const minusMonth = () => {
-    setNum(num - 1)
-  }
-
-  useEffect(() => {
-    getStandardDate()
-  },[num])
-
-  useEffect(() => {
+  const calculationDate = useCallback(() => {
     const startMonth = moment(`${dashStandard}-01`).format('YYYY-MM-DD')
     const startDay = moment(startMonth).day()
 
@@ -114,22 +57,91 @@ const Calendar = () => {
       startDay: startDay,
       endDay: endDay
     })
-  },[dashStandard])
+  },[dashStandard, monthDate, monthDay])
 
-  useEffect(() => {
-    test()
-  },[monthDate])
+  // 예약 가능 날짜 판단
+  const selectAbleDate = useCallback((i) => {
+    if (list[i] === '0') {
+      return false
+    } else {
+      return true
+    }
+  },[])
 
-  const getDate = (date, isOrigin, isAble) => {
+  // 달력에 보여줄 날짜 계산
+  const getRenderDate = useCallback(() => {
+    const { startDate, endDate } = monthDate
+    const { startDay, endDay } = monthDay
+    const arr = []
+    const prevArr = []
+    const nextArr = []
+    const period = moment(endDate).diff(moment(startDate), "days");
+
+    //해당 달에 대한 계산
+    for(let i = 0; i <= period; i++) {
+      let differencePeriod = moment(startDate).add(i, "d").format("DD");
+      // date: 날짜, origin: 해당 월의 날짜 인지 판단, able: 예약 가능한 날짜 인지 판단
+      arr.push({
+        date: differencePeriod,
+        origin: true,
+        able: selectAbleDate(i)
+      })
+    }
+
+    // 해당 달의 시작 요일 이전 날짜 계산
+    for(let i = 1; i <= startDay; i++) {
+      let pervPeriod = moment(startDate).add(-i, 'd').format('DD')
+      // date: 날짜, origin: 해당 월의 날짜 인지 판단, able: 예약 가능한 날짜 인지 판단
+      prevArr.unshift({
+        date: pervPeriod,
+        origin: false,
+        able: false
+      })
+    }
+
+    // 해당 달의 마지막 요일 이후의 날짜 계산
+    for(let i = 1; i <= 6 - endDay; i++) {
+      let nextPeriod = moment(endDate).add(i, 'd').format('DD')
+      // date: 날짜, origin: 해당 월의 날짜 인지 판단, able: 예약 가능한 날짜 인지 판단
+      nextArr.push({
+        date: nextPeriod,
+        origin: false,
+        able: false
+      })
+    }
+
+    // 달력 데이터 업데이트
+    setDateArr([...prevArr, ...arr, ...nextArr])
+  },[monthDate, monthDay, selectAbleDate])
+
+  // 상단 헤더 년도+날짜 계산
+  const getStandardDate = useCallback(() => {
+    const year = moment(TODAY).add(num, 'M').format('YYYY')
+    const month = moment(TODAY).add(num, 'M').format('MM')
+    // 달력 헤더
+    setStandard(`${year}년 ${month}월`)
+
+    // 날짜 계산을 위한 dash 기준
+    setDashStandard(`${year}-${month}`)
+  },[num, standard, dashStandard])
+
+  const addMonth = useCallback(() => {
+    setNum(num + 1)
+  },[num])
+
+  const minusMonth = useCallback(() => {
+    setNum(num - 1)
+  },[num])
+
+  const getDate = useCallback((date, isOrigin, isAble) => {
     if(isAble) {
       getSelectDate(date)
     } else {
       console.log(date)
     }
+  },[selectDate])
 
-  }
-
-  const getSelectDate = (date) => {
+  const getSelectDate = useCallback((date) => {
     const { startDate, endDate } = selectDate
     if(!startDate && !endDate) {
       setSelectDate({
@@ -152,19 +164,9 @@ const Calendar = () => {
         endDate: ''
       })
     }
-  }
+  },[selectDate])
 
-  // console.log(selectDate)
-  const selectAbleDate = (i) => {
-    if(list[i] === '0') {
-      return false
-    } else {
-      return true
-    }
-  }
-
-  // console.log(selectAbleDate())
-  const makeDate = (index, date) => {
+  const makeDate = useCallback((index, date) => {
     const year = moment(TODAY).add(num, 'M').format('YYYY')
     const prevYear = moment(year).add(-1, 'y').format('YYYY')
     const nextYear = moment(year).add(1, 'y').format('YYYY')
@@ -190,27 +192,43 @@ const Calendar = () => {
     } else {
       return `${dashStandard}-${date}`
     }
-  }
+  },[monthDay, dashStandard])
 
-  console.log(dateArr)
-  const renderDate = () => {
-    return dateArr.map((el, index) => {
-      // const dashDate = index < monthDay.startDay  || index >= (dateArr.length - (6 - monthDay.endDay))? el.date : `${dashStandard}-${el.date}`
-      const dashDate = makeDate(index, el.date)
-      const isOrigin = el.origin ? '' : 'grayed'
-      const isAble = el.able ? '' : 'grayed'
-      const selected = selectDate.startDate === dashDate ? 'selected' : ''
-      const selected2 = selectDate.endDate === dashDate ? 'selected' : ''
-      const able = el.able ? 'able' : ''
+  useEffect(() => {
+    getStandardDate()
+  },[num])
+
+  useEffect(() => {
+    calculationDate()
+  },[dashStandard])
+
+  useEffect(() => {
+    getRenderDate()
+  },[monthDate])
+
+  // 날짜 렌더 map 함수
+  const renderDate = useCallback(() => {
+    if(dateArr.length === 0 || dateArr === null) {
+      return;
+    } else {
+      return dateArr.map((el, index) => {
+        // const dashDate = index < monthDay.startDay  || index >= (dateArr.length - (6 - monthDay.endDay))? el.date : `${dashStandard}-${el.date}`
+        const dashDate = makeDate(index, el.date)
+        const isOrigin = el.origin && el.able ? '' : 'grayed'
+        const isAble = el.origin && !el.able ? 'notAble' : ''
+        const selected = selectDate.startDate === dashDate ? 'selected' : ''
+        const selected2 = selectDate.endDate === dashDate ? 'selected' : ''
+        const able = el.able ? 'able' : ''
 
         return (
-            <div className={`box ${isOrigin} ${isAble} ${selected} ${selected2}`} key={index} onClick={() => getDate(dashDate, el.origin, el.able)}>
-              <span className={`text`}>{el.date}</span>
-              <div className={`${able}`}></div>
-            </div>
+          <div className={`box ${isOrigin} ${isAble} ${selected} ${selected2}`} key={index} onClick={() => getDate(dashDate, el.origin, el.able)}>
+            <span className={`text`}>{el.date}</span>
+            <div className={`${able}`}></div>
+          </div>
         )
-    })
-  }
+      })
+    }
+  },[dateArr, selectDate])
 
   return (
     <div className="Calendar">
@@ -247,8 +265,6 @@ const Calendar = () => {
           {renderDate()}
         </div>
       </div>
-
-
     </div>
   )
 }
